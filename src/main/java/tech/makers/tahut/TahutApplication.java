b48a38d5b81146d26d9c35f218f68348add04d4d
Viktor Vrecko <vvrecko@posteo.de>
2022-02-21 15:27:27 +0100

    Initial

---
+++ src/main/java/tech/makers/tahut/TahutApplication.java
@@ -0,0 +1,13 @@
+package tech.makers.tahut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TahutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TahutApplication.class, args);
+	}
+
+}
